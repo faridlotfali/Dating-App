101eba8ae1ad9ee5720428eccac1d9762f050c0d
faridlotfali <farid9lotfali@gmail.com>
2020-01-16 00:17:17 +0330

    Initial Commit

---
+++ Data/DataContext.cs
@@ -0,0 +1,12 @@
+using DatingApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options){}
+
+        public DbSet<Values> Values { get; set; }
+    }
+}
---
+++ Models/Values.cs
@@ -0,0 +1,8 @@
+namespace DatingApp.Models
+{
+    public class Values
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
